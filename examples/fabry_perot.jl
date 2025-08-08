9de823cdd96177817e1da9a0fd40322a4187fb70
Jeffrey Wack <jeffwack111@gmail.com>
2025-08-08 11:58:21 -0700

    add simple Fabry-Perot example

---
+++ examples/fabry_perot.jl
@@ -0,0 +1,15 @@
+# Here we define a one-sided Fabry-Perot cavity as an SLH system, and perform some simple
+# numerical experiments on it to demonstrate basic simulations with physical
+# parameters
+
+using SecondQuantizedAlgebra
+using SLHQuantumSystems
+
+hilb = FockSpace(:cav)
+a = Destroy(hilb, :a)
+
+@cnumbers ω κ
+
+cavity = SLH(:cav,[:in],[:out],[1],[√κ*a],ω*a'*a)
+
+
