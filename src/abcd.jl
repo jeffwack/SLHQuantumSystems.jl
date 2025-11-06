6802596bf425043eca78f33c6210c00991f4b2e9
Jeffrey Wack <jeffwack111@gmail.com>
2025-11-06 12:04:42 -0800

    implemented Combes eq 116-119 for SLH to StateSpace (untested)

--- src/abcd.jl
+++ src/abcd.jl
@@ -8,16 +8,6 @@ struct StateSpace
     D
 end
 
-struct PassiveStateSpace
-    name
-    inputs
-    outputs
-    A
-    B
-    C
-    D
-end
-
 function StateSpace(sys::SLH)
 
     S = sys.S
@@ -42,17 +32,53 @@ function StateSpace(sys::SLH)
     # We create a vector which contains the creation and annihilation operators
     # of the system. For now we assume we are in that basis rather than
     # quadratures.
-    x = filter!(q->q isa Destroy,state_vector(H))
+    x = filter(q->q isa Destroy,state_vector(H))
     m = length(x)
-
+    
     # Now we go through every pair of annihilation operators and look for the
     # corresponding terms in the Hamiltonian
     terms = get_additive_terms(H)
     args = [SymbolicUtils.arguments(term) for term in terms]
+    #println(args)
+
+    # We want to separate our terms into energy conserving (omegaminus) and non-conserving (omegaplus).
+    # The operator ordering of second quantized algebra is only applied to non-commuting operators.
     
     omegaminus = Array{Any}(zeros(m, m))
-    #omegaminus = Array{Any}(zeros(m, m))
+    omegaplus = Array{Any}(zeros(m, m))
 
+    #organized by term
+    for term in args
+        # first we will check that this term is quadratic in the operators
+        if length(filter(q->q isa SecondQuantizedAlgebra.QNumber,term)) != 2
+            return error("$term of the Hamiltonian is not quadratic")
+        end
+        #now we can determine what 'kind' of term this is by counting the number of creation operators
+        creators = filter(q->q isa SecondQuantizedAlgebra.Create,term)
+        destroyers = filter(q->q isa SecondQuantizedAlgebra.Destroy,term)
+        if length(creators) == 1
+            cr = first(creators)
+            j = first(findall(q->q==cr',x))
+
+            de = first(destroyers)
+            k = first(findall(q->q==de,x))
+
+            omegaminus[j,k] = first(filter(arg -> arg isa SymbolicUtils.BasicSymbolic, term))
+        elseif length(creators) == 2 && creators[1] == creators[2]
+            cr = first(creators)
+            j = first(findall(q->q==cr',x))
+            omegaplus[j,j] = first(filter(arg -> arg isa SymbolicUtils.BasicSymbolic, term))
+        elseif length(creators) == 0 && destroyers[1] == destroyers[2]
+            de = first(destroyers)
+            j = first(findall(q->q==de,x))
+            #If H is Hermitian then this is already handled
+        else
+            return error("don't know how to handle $term")
+        end
+    end
+
+    #organized by operator
+    #=
     for (j,aj) in enumerate(x)
         filteredterms = filter(list -> aj' in list, args)
         for (k, ak) in enumerate(x)
@@ -62,29 +88,80 @@ function StateSpace(sys::SLH)
             omegaminus[j,k] = coef
         end
     end
-
+    =#
     #Now we parse L
-    phiminus = Array{Any}(zeros(length(L), m))
+    n = length(L)
+    phiminus = Array{Any}(zeros(n, m))
+    phiplus = Array{Any}(zeros(n,m))
 
     for (j,op) in enumerate(L)
         terms = get_additive_terms(op)
         args = [SymbolicUtils.arguments(term) for term in terms]
-        for (k, ak) in enumerate(x)
-            term = first(filter(list -> ak in list,args))
-            coef = first(filter(arg -> arg isa SymbolicUtils.BasicSymbolic, term))
-            phiminus[j,k] = coef
+        for term in args
+            #first make sure each term is linear in operators
+            ops = filter(q->q isa SecondQuantizedAlgebra.QNumber,term)
+            if length(ops) != 1
+                return error("$term of L is not linear")
+            end
+            op = first(ops)
+            if op isa SecondQuantizedAlgebra.Destroy
+                k = first(findall(q->q==op,x))
+                phiminus[j,k] = first(filter(arg -> arg isa SymbolicUtils.BasicSymbolic, term))
+            else
+                k = first(findall(q->q==op',x))
+                phiplus[j,k] = first(filter(arg -> arg isa SymbolicUtils.BasicSymbolic,term))
+            end
         end
     end
-    
-    newS = 
 
-    A = -0.5*phi'*phi - 1.0im*omega
-    B = -omega'*S
-    C = omega
-    D = S
-
-    return PassiveStateSpace(sys.name, sys.inputs,sys.outputs, A,B,C,D)
+    #Now we need to construct the matrices according to Combes eqs 116-119
     
+    Phi = [phiminus phiplus;
+            conj.(phiplus) conj.(phiminus)]
+
+    Omega = [omegaminus omegaplus;
+            conj.(omegaplus) conj.(omegaminus)]
+
+    
+    A = -0.5*J(m)*Phi'*J(n)*Phi - 1.0im*Omega
+    D = [S zeros(size(S));
+        zeros(size(S)) conj.(S)]
+    B = -J(m)*Phi'*J(n)*D
+    C = Phi
+    
+
+    #Finally we permute the matrices for our convention of 'interlaced' rather than 'doubled up' creation and annihilation operators
+    A = P(m)*A*P(m)'
+    B = P(m)*B*P(n)'
+    C = P(n)*C*P(m)'
+    D = P(n)*D*P(n)'
+
+    return StateSpace(sys.name, sys.inputs,sys.outputs, A,B,C,D)
+    
+end
+
+function J(n::Int)
+    Id = Matrix{Int}(I, n, n)
+    Ze = Matrix{Int}(zeros(n, n))
+    return [Id Ze;
+            Ze -Id]
+end
+
+function P(n::Int)
+    # Create permutation matrix
+    PP = zeros(Int, 2n, 2n)
+    
+    # Map annihilation operators: position i → position 2i-1
+    for i in 1:n
+        PP[2i-1, i] = 1
+    end
+    
+    # Map creation operators: position n+i → position 2i
+    for i in 1:n
+        PP[2i, n+i] = 1
+    end
+    
+    return PP
 end
 """
     slh2abcd(sys::SLH)
