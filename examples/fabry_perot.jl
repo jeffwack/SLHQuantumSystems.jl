83a8cc917097c262382fc917b70860657d366575
Jeffrey Wack <jeffwack111@gmail.com>
2025-11-12 17:33:27 -0800

    small fixes for bode plot sizing

--- examples/fabry_perot.jl
+++ examples/fabry_perot.jl
@@ -10,12 +10,12 @@ a = Destroy(hilb, :a)
 
 cav = SLH(:cav,[:in_L, :in_R],[:out_L, :out_R],[1 0; 0 1],[√κ_L*a, √κ_R*a],ω*a'*a)
 
-cavSS = slh2abcd(cav)
+cavSS = StateSpace(cav)
 
 quadcavSS = toquadrature(cavSS)
 
-aasymbtfs = symbfresponse(cavSS)
-quadsymbtfs = symbfresponse(quadcavSS)
+#aasymbtfs = symbfresponse(cavSS)
+#quadsymbtfs = symbfresponse(quadcavSS)
 
 paramdict = Dict([ω => 10, κ_L => 3, κ_R => 2])
 
--- ext/MakieExt.jl
+++ ext/MakieExt.jl
@@ -12,10 +12,10 @@ function SLHQuantumSystems.bode(sys::StateSpace,input,output,freq)
 
     tf = tfs[i,j]
 
-    fig = Figure(size = (600,400))
+    fig = Figure(size = (800,500))
     title = Label(fig[1,1], "Frequency Response of $(sys.name)")
-    magax = Axis(fig[2,1],width = 600,height = 300,xscale=log10,yscale=log10,xticksvisible=false,xticklabelsvisible=false)
-    phaseax = Axis(fig[3,1],width = 600, height = 100,xscale=log10)
+    magax = Axis(fig[2,1],width = 700,height = 300,xscale=log10,yscale=log10,xticksvisible=false,xticklabelsvisible=false)
+    phaseax = Axis(fig[3,1],width = 700, height = 100,xscale=log10)
     linkxaxes!(magax,phaseax)
 
     scatter!(magax,freq,abs.(tf),label = "$input -> $output")
